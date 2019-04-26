5130e3f513e2296b31e3fd25257288b61e558ae4
Maximiliano <mopitz199@gmail.com>
2019-04-25 22:46:33 -0400

    Add files via upload

---
+++ test2/index.php
@@ -0,0 +1,30 @@
+<html>
+    <head></head>
+    <body>
+        <?php
+            $database_name = "test1";
+            $username = "test1";
+            $password = "test1";
+            $server = "localhost";
+
+            $link = mysqli_connect($server, $database_name, $username, $password);
+
+            if (!$link) {
+                echo "Error: Unable to connect to MySQL." . PHP_EOL;
+                echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+                echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+                exit;
+            }else{
+                echo "Successfull connect";
+
+                $sql = "INSERT INTO person (name) VALUES ('John')";
+
+                if ($link->query($sql) === TRUE) {
+                    echo "New record created successfully";
+                } else {
+                    echo "Error: " . $sql . "<br>" . $link->error;
+                }
+            }     
+        ?>
+    </body>
+</html>
